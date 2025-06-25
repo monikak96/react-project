4bedbd8447b26c9961c244cf52981ba877313ae3
monikak96 <monikakrishna96@gmail.com>
2025-06-25 19:25:13 +0530

    Use React Hooks to filter data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,33 @@
 import RestaurantCard from "./RestaurantCard";
-import resList  from "../utils/mockData";
+import resList from "../utils/mockData";
+
+import { useState } from "react";
 
 const Body = () => {
-    return (
-        <div className="body">
-            <div className="search">Search bar</div>
-            <div className="res-container">
-                {resList.map(restaurant=><RestaurantCard key={restaurant.info.id}resData={restaurant}/>)}
-            </div>
-        </div>
-    )
-}
+  const arr = useState(resList)
+  console.log(arr);
+  const [restaurantList,setRestaurantList] = arr
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            let filteredList = restaurantList.filter(restaurant=>restaurant.info.avgRating < 4.4)
+            console.log(filteredList)
+            setRestaurantList(filteredList)
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {restaurantList.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Body;
