b881de351b77eeb949045fd5f54080626724c06b
monikak96 <monikakrishna96@gmail.com>
2025-06-26 00:01:22 +0530

    Add Login Logout using react hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,30 +1,47 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const arr = useState(resList)
-  console.log(arr);
-  const [restaurantList,setRestaurantList] = arr
+  const [restaurantList,setRestaurantList] = useState([])
+
+  useEffect(()=>{
+    fetchData()
+  },[])
+
+  const fetchData = () =>{
+    // you can change the restauarant list from here , but swiggys api's dont work anymore
+    setRestaurantList(resList)
+  }
+  
+
+  if(restaurantList.length === 0){
+    return <Shimmer/>
+  }
+  
   return (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
-            let filteredList = restaurantList.filter(restaurant=>restaurant.info.avgRating < 4.4)
-            console.log(filteredList)
-            setRestaurantList(filteredList)
+            let filteredList = restaurantList.filter(
+              (restaurant) => restaurant.info.avgRating < 4.4
+            );
+            console.log(filteredList);
+            setRestaurantList(filteredList);
           }}
         >
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {restaurantList.map((restaurant) => (
+        {
+        restaurantList.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
-        ))}
+        ))
+        }
       </div>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,33 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+    const [loginbtn,setLoginButton] = useState("Login")
     return (
-        <div className="header">
-            <div className="logo">
-                <img className="logo-img" src={LOGO_URL}/>
-            </div>
-            <div className="nav-items">
-                <ul>
-                    <li>About</li>
-                    <li>Contact</li>
-                    <li>Blog</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
+      <div className="header">
+        <div className="logo">
+          <img className="logo-img" src={LOGO_URL} />
         </div>
-    )
+        <div className="nav-items">
+          <ul>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Blog</li>
+            <li>Cart</li>
+          </ul>
+          <button
+            className="login"
+            onClick={() => {
+              loginbtn === "Login"
+                ? setLoginButton("Logout")
+                : setLoginButton("Login");
+            }}
+          >
+            {loginbtn}
+          </button>
+        </div>
+      </div>
+    );
 }
 
 export default Header;
