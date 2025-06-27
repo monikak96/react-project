b3e3d718e651e00dcb7f6a2a333be3122e535e8e
monikak96 <monikakrishna96@gmail.com>
2025-06-27 23:41:47 +0530

    Use a class based component instead of fnctnl for Contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,28 @@
-const Contact = () => {
+import { Component } from "react";
+import UserClass from "./UserClass";
+
+class Contact extends Component {
+    
+  constructor(props) {
+    super(props);
+    console.log("parent Constructor");
+  }
+
+  componentDidMount(){
+    console.log("Parent Did Mount")
+  }
+
+  render() {
+    console.log("Parent rendered")
     return (
-        <div>
-            <h1> Contact</h1>
-            <h2>Hello In case of Any queries contact us at test@test.com</h2>
-        </div>
-    )
+      <div className="contact">
+        <h1> Contact</h1>
+        <h2>Hello In case of Any queries contact us at test@test.com</h2>
+        <h2 className="contact-card">Our Team</h2>
+        <UserClass name={"Mounika"} location={"Hyderabad"} />
+      </div>
+    );
+  }
 }
 
 export default Contact;
---
+++ src/components/UserClass.js
@@ -0,0 +1,50 @@
+import { Component } from "react";
+class UserClass extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+    console.log("Child constructor")
+  }
+
+  componentDidMount(){
+    console.log("Child Did Mount")
+  }
+  render() {
+    const { name, location } = this.props;
+    const { count } = this.state;
+    console.log("Child rendered");
+    
+    return (
+      <div className="user-card">
+        <h2>Name : {name}</h2>
+        <h2>
+          Count : {count}{" "}
+          <button
+            onClick={() => {
+              this.setState({
+                count: this.state.count + 1,
+              });
+            }}
+          >
+            Increment count
+          </button>
+          <button
+            onClick={() => {
+              this.setState({
+                count: 0,
+              });
+            }}
+          >
+            reset
+          </button>
+        </h2>
+        <h2>Location : {location}</h2>
+        <h2>Email : bunpi@gmail.com</h2>
+      </div>
+    );
+  }
+}
+
+export default UserClass;
