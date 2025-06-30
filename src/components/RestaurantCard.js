32ff67e6f480e43766916c910db28d315eb4a23e
monikak96 <monikakrishna96@gmail.com>
2025-06-30 13:04:00 +0530

    Just code formatting

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,7 +1,10 @@
-const RestaurantCard = (props) =>{
-    let {resData} = props
-    let {name,cuisines,avgRating,costForTwo,sla} = resData?.info;
-    let cloudinaryImageId = "https://www.foodiesfeed.com/wp-content/uploads/ff-images/2025/01/delicious-mushroom-soup-with-fresh-herbs.png"
+const RestaurantCard = (props) => {
+    
+    let { resData } = props
+    let { name,cuisines,avgRating,costForTwo,sla } = resData?.info;
+
+    let cloudinaryImageId = "https://www.foodiesfeed.com/wp-content/uploads/ff-images/2025/01/delicious-mushroom-soup-with-fresh-herbs.png";
+
     return(
         <div className="res-card w-[250] h-[360] p-2 m-2 text-left bg-orange-50 hover:bg-orange-200 rounded-lg">
             <img className="rounded-lg "  src={cloudinaryImageId}/>
@@ -9,7 +12,7 @@ const RestaurantCard = (props) =>{
             <h5 className="font-medium text-m truncate p-1">{cuisines.join(", ")}</h5>
             <h5 className="font-medium text-m p-1">{costForTwo}</h5>
             <h5 className="font-medium text-m p-1">{avgRating}</h5>
-            <h6 className="font-medium  text-m p-1">{sla.deliveryTime} minutes</h6>  
+            <h6 className="font-medium  text-m p-1">{sla.deliveryTime} minutes</h6>
         </div>
     )
 }
