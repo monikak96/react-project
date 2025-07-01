4d8f6df82342ae0dc6e0a17472ac335bf34f840c
monikak96 <monikakrishna96@gmail.com>
2025-07-01 11:16:10 +0530

    Use default userContext value using provider and add a useState to update userContext

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useContext, useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-// import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
 
 import { RouterProvider, createBrowserRouter, Outlet } from "react-router";
 import RestaurantMenu from "./components/RestaurantMenu";
-// import Grocery from "./components/Grocery";
 
 const Grocery = lazy(() => import("./components/Grocery"));
-const About = lazy(()=> import("./components/About"));
+const About = lazy(() => import("./components/About"));
 
-const AppComponent = () => (
-  <div className="main">
-    <Header />
-    <Outlet />
-  </div>
-);
+import UserContext from "./utils/UserContext";
+
+const AppLayout = () => {
+
+  const [userName, setUserName] = useState();
+
+  // authentication
+  useEffect(() => {
+    const data = {
+      name: "Mounika",
+    };
+    setUserName(data.name);
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
+      <div className="main">
+        <Header />
+        <Outlet />
+      </div>
+    </UserContext.Provider>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <AppComponent />,
+    element: <AppLayout />,
     errorElement: <Error />,
     children: [
       {
@@ -34,7 +50,11 @@ const appRouter = createBrowserRouter([
       },
       {
         path: "/about",
-        element: <Suspense fallback={<h1>This is also a fallback fn</h1>}><About /></Suspense>
+        element: (
+          <Suspense fallback={<h1>This is also a fallback fn</h1>}>
+            <About />
+          </Suspense>
+        ),
       },
       {
         path: "/contact",
